f4b00603526dd2b47d329435ab104a9199f1abcd
Dathan <dathan@gmail.com>
2015-08-31 13:34:35 -0700

    Extract git_verify_repo and add git_checkout_regex

--- zsh/git.zsh
+++ zsh/git.zsh
@@ -18,34 +18,36 @@ ggr() {
   pwd
 }
 
-git_current_feature() {
-  local _BRANCH
-  local _FEATURE
-  _BRANCH=`git rev-parse --abbrev-ref HEAD`
-  if [[ ! $_BRANCH =~ "feature" ]]; then
-    echo "Not on a feature"
+git_verify_repo() {
+  git rev-parse --git-dir 2>/dev/null >/dev/null
+  if [[ $? != 0 ]]; then
+    echo "$PWD is not a git repository"
     return 1
   fi
+}
 
+git_current_feature() {
+  git_verify_repo || return 1
+
+  local _BRANCH
+  local _FEATURE
+
+  _BRANCH=`git rev-parse --abbrev-ref HEAD`
   _FEATURE=`sed -e 's/^feature\///' <<< $_BRANCH`
+
   echo $_FEATURE
 }
 
 git_delete_branch() {
-  local _all_branches
+  git_verify_repo || return 1
+  local _branches
 
-  _all_branches=$( command git branch )
-  if [ "$?" -ne 0 ]; then
-    echo "Failed; are you in a Git repository?"
-    return 1
-  fi
-
-  _all_branches=( $( cut -c 3- <<< $_all_branches ) )
+  _branches=( $( command git branch | cut -c 3- ) )
 
   local _matching_branches
   _matching_branches=()
 
-  for b in "${_all_branches[@]}"; do
+  for b in "${_branches[@]}"; do
     if [[ "$b" =~ "$1" ]]; then
       _matching_branches+=("$b")
     fi
@@ -67,7 +69,38 @@ git_delete_branch() {
   git branch -d "$_matching_branches[1]"
 }
 
+git_checkout_regex() {
+  git_verify_repo || return 1
+
+  local _branches
+  _branches=( $( command git branch | cut -c 3- ) )
+
+  local _matching_branches
+  _matching_branches=()
+
+  for b in "${_branches[@]}"; do
+    if [[ "$b" =~ "$1" ]]; then
+      _matching_branches+=("$b")
+    fi
+  done
+
+  if [[ ${#_matching_branches[@]} == 0 ]]; then
+    echo "No branches matched expression '$1'"
+    return 1
+  elif [[ ${#_matching_branches[@]} > 1 ]]; then
+    echo "Multiple branches matched expression '$1':"
+    for b in "${_matching_branches[@]}"; do
+      echo $b
+    done
+    return 2
+  fi
+
+  git checkout "$_matching_branches[1]"
+
+}
+
 alias gffs="git flow feature start"
 alias gfff="git flow feature finish"
 alias gfffc="git flow feature finish \`git_current_feature\`"
 alias gdbr="git_delete_branch"
+alias gcor="git_checkout_regex"
