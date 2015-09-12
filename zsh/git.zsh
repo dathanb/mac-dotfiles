0bc4668b6d071e727ad1ceddd5d21c419e4709a0
Dathan <dathan@gmail.com>
2015-09-11 17:14:28 -0700

    Add docs to git file

--- zsh/git.zsh
+++ zsh/git.zsh
@@ -1,7 +1,7 @@
-# setup git aliases
-alias gst='git status --short'
-alias gpu='git push --set-upstream origin `git rev-parse --abbrev-ref HEAD`'
-#alias ggr='TMP_REVPARSE=`git rev-parse --show-toplevel` && cd "$_TEMP_REVPARSE" && pwd'
+# 'Goto Git Root'
+# Will change directory to the root of the current git repo
+# Also prints the directory, so you can easily use it in a subshell to get the path of your repo
+# e.g., cat `ggr`/.gitignore
 ggr() {
   local _TMP_REVPARSE
   local RET_VAL
@@ -18,6 +18,7 @@ ggr() {
   pwd
 }
 
+# Returns failure if the current directory is not part of a git repository
 git_verify_repo() {
   git rev-parse --git-dir 2>/dev/null >/dev/null
   if [[ $? != 0 ]]; then
@@ -26,6 +27,8 @@ git_verify_repo() {
   fi
 }
 
+# For gitflow, returns the name of the current feature
+# e.g., if your current branch is "feature/some_feature_name", returns "some_feature_name"
 git_current_feature() {
   git_verify_repo || return 1
 
@@ -38,6 +41,10 @@ git_current_feature() {
   echo $_FEATURE
 }
 
+# Matches the set of local branches against the provided regex
+# If there's no match, fails
+# If there are multipe matches, prints the matching branches and fails
+# If there's a single match, runs git branch -d matching_branch_name
 git_delete_branch() {
   git_verify_repo || return 1
   local _branches
@@ -69,6 +76,10 @@ git_delete_branch() {
   git branch -d "$_matching_branches[1]"
 }
 
+# Matches the set of local branches against the provided regex
+# If there's no match, fails
+# If there are multiple matches, prints the matching branches and fails
+# If there's a single match, runs git checkout matching_branch_name
 git_checkout_regex() {
   git_verify_repo || return 1
 
@@ -99,8 +110,14 @@ git_checkout_regex() {
 
 }
 
+
+alias gst='git status --short'
+# GPU == "Git Push Upstream"
+alias gpu='git push --set-upstream origin `git rev-parse --abbrev-ref HEAD`'
 alias gffs="git flow feature start"
 alias gfff="git flow feature finish"
 alias gfffc="git flow feature finish \`git_current_feature\`"
+# GBDR == "Git Branch Delete with Regex"
 alias gbdr="git_delete_branch"
+# GCOR == "Git CheckOut with Regex
 alias gcor="git_checkout_regex"
