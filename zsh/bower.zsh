410c7c21e798023ccaf25d5e40334cf9cd62cc06
Dathan <dathan@gmail.com>
2015-09-11 17:07:03 -0700

    Add readme to grunt and bower

--- zsh/bower.zsh
+++ zsh/bower.zsh
@@ -1,3 +1,7 @@
+# I don't like to install bower globally, but I also don't like to have to type
+# ./node_modules/bower/bin/bower or ./node_modules/.bin/bower
+# So this function now allows you to just type "bower" and use the local instal if it exists,
+# otherwise failover to the global-installed version, if it exists
 if [[ -n "$(alias bower)" ]]; then
   unalias bower
 fi
--- zsh/grunt.zsh
+++ zsh/grunt.zsh
@@ -1,3 +1,8 @@
+# I don't like to install grunt globally, but I also don't like to have to type
+# ./node_modules/grunt/bin/bower or ./node_modules/.bin/bower
+# So this function now allows you to just type "grunt" and use the local instal if it exists,
+# otherwise failover to the global-installed version, if it exists
+
 if [[ -n "$(alias grunt)" ]]; then
   unalias grunt
 fi
