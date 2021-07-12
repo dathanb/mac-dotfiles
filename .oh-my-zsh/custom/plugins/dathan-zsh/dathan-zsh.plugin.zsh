211fa64b7243f4b8f7aa387721773a32e7cc408b
Dathan Bennett <dathan@gmail.com>
2021-07-12 16:09:23 -0700

    Add alias :q=exit as a crutch beceause I have vim muscle memory

--- .oh-my-zsh/custom/plugins/dathan-zsh/dathan-zsh.plugin.zsh
+++ .oh-my-zsh/custom/plugins/dathan-zsh/dathan-zsh.plugin.zsh
@@ -19,6 +19,8 @@ setopt HIST_REDUCE_BLANKS        # Remove superfluous blanks before recording en
 setopt HIST_VERIFY               # Don't execute immediately upon history expansion.
 setopt HIST_BEEP                 # Beep when accessing nonexistent history.
 
+alias :q=exit
+
 # I don't like RPROMPT, because I copy from the terminal a lot and it makes that inconvenient
 unset RPROMPT
 
