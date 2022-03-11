a4e3047d75d04a4d29c7073c0b62fa4a0293342a
Dathan Bennett <dathan@gmail.com>
2022-03-10 20:37:28 -0800

    Update git config and add README

---
+++ README.md
@@ -0,0 +1,5 @@
+In addition to these, I installed [diff-so-fancy](https://github.com/so-fancy/diff-so-fancy) with their default config.
+
+And [fx](https://github.com/antonmedv/fx)
+
+And [fzf](https://github.com/junegunn/fzf)
--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # copy everything except the .git folder to the home directory
-for f in `ls -a | grep -v '^\.$' | grep -v '^\.\.$' | grep -v '^.git$' | grep -v '^install.sh$'`; do
+for f in `ls -a | grep -v "README.md" | grep -v '^\.$' | grep -v '^\.\.$' | grep -v '^.git$' | grep -v '^install.sh$'`; do
     cp -r -v $f ~/
 done
